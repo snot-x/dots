2249a752644aa486bcef31fed26336795e505575
snot-x <snot@humanoid.net>
2025-08-19 18:55:30 -0300

    uhh hopefully this works; symlinks are a bitch

--- hypr/components/fish/theme.fish
+++
@@ -1,9 +0,0 @@
-function theme --on-signal USR2
-    theme.sh < $HOME/.config/hypr/colors
-end
-
-if status is-interactive
-	pokemon-colorscripts -r --no-title &
-	starship init fish | source &
-	theme
-end
